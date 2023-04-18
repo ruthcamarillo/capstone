d5804eb4733d19a52b98e00e34bf8a07d194ef01
BreCode13 <119532759+BreCode13@users.noreply.github.com>
2023-04-18 11:52:47 -0400

    working on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,3 +35,38 @@ function Footer() {
 }
 
 export default Footer;
+
+// import React from 'react';
+// // import './DisasterFooter.css';
+
+// function Footer() {
+//   return (
+//     <footer className="disaster-footer">
+//       <div className="disaster-footer-content">
+//         <div className="disaster-footer-section">
+//           <h3>Emergency Contacts</h3>
+//           <p>Call 911 for immediate assistance in case of emergency.</p>
+//           <p>For non-emergency assistance, call (555) 555-5555.</p>
+//         </div>
+//         <div className="disaster-footer-section">
+//           <h3>Get Involved</h3>
+//           <p>Donate to help support disaster relief efforts.</p>
+//           <button className="disaster-footer-button">Donate Now</button>
+//         </div>
+//         <div className="disaster-footer-section">
+//           <h3>Follow Us</h3>
+//           <ul className="disaster-footer-social">
+//             <li><a href="#">Facebook</a></li>
+//             <li><a href="#">Twitter</a></li>
+//             <li><a href="#">Instagram</a></li>
+//           </ul>
+//         </div>
+//       </div>
+//       <div className="disaster-footer-attribution">
+//         <p>&copy; 2023 Disaster Dispatch. All Rights Reserved.</p>
+//       </div>
+//     </footer>
+//   );
+// }
+
+// export default DisasterFooter;
