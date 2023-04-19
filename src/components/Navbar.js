3b17065888201ebc998ef2627e3a531831267ee5
JorgeZmedina <114126984+JorgeZmedina@users.noreply.github.com>
2023-04-19 17:48:51 -0400

    fixing up code and nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,38 @@
 import React from "react";
 import '../styles/NavBar.css'
 import { Link } from "react-router-dom";
+
 // BrowserRouter, Route,
 
 function Navbar() {
+
     return (
 
-
-        <div className="nav-container">
+        
+        <div className="nav nav-container">
+            <div className="logo"><Link to="/"><img src="/logo.png" alt="logo" class="logo"></img></Link></div>
             <nav>
                 <ul>
-
                     <li class="nav-item"> 
-                    {/* <a> */}
                          <Link to="/">Home</Link> 
-                         {/* </a> */}
                          </li>
 
                     <li class="nav-item">
-                         {/* <a> */}
                         <Link to="/contact">Contact</Link> 
-                        {/* </a>  */}
                         </li>
 
                     <li class="nav-item"> 
-                    {/* <a> */}
                          <Link to="/about">About</Link> 
-                    {/* </a>  */}
                          </li>
-
-                <li class="nav-item"> 
-                <a>
-                     <Link to="/about">About</Link> 
-                     </a> 
-                     </li>
 
                 <li class="nav-item"> <a> <Link to="/Resources">Resources</Link> </a> </li>
 
                 <li class="nav-item"> <a> <Link to="/Signup">Volunteer</Link> </a> </li>
 
-
             </ul>
         </nav>
 
-
-</div>
-
+    </div>
     );
 }
 
