8d5bea5d1ec39c7160ebc5cb0aebfe7f9fc62031
JorgeZmedina <114126984+JorgeZmedina@users.noreply.github.com>
2023-04-19 17:48:51 -0400

    fixing up code and nav bar

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+// BrowserRouter, Route,
 import '../styles/NoPage.css'
 
 
