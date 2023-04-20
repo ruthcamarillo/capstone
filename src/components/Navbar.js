4c4858fdb1c8adc23220c535d0414ddcbe4c8a0a
BreCode13 <119532759+BreCode13@users.noreply.github.com>
2023-04-20 13:36:58 -0400

    added to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,13 +29,16 @@ function Navbar() {
                     {/* </a>  */}
                          </li>
 
-                    <li class="nav-item"> 
-                    {/* <a>  */}
-                        <Link to="/Resources">Resources</Link> 
-                        {/* </a>  */}
-                        </li>
+                <li class="nav-item"> 
+                <a>
+                     <Link to="/about">About</Link> 
+                     </a> 
+                     </li>
 
-                        <li class="nav-item"> <a> <Link to="/Signup">Volunteer</Link> </a> </li>
+                <li class="nav-item"> <a> <Link to="/Resources">Resources</Link> </a> </li>
+
+                <li class="nav-item"> <a> <Link to="/Signup">Volunteer</Link> </a> </li>
+
 
             </ul>
         </nav>
