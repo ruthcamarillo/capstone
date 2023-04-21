097a9ea131db3e82e46db2983bdbfbc7f01ef5eb
JorgeZmedina <114126984+JorgeZmedina@users.noreply.github.com>
2023-04-20 23:47:37 -0400

    added logo, nav bar, and some about page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,1 +1,57 @@
-// import React from "react";
+import React, { useState, useEffect } from "react";
+
+const Card = [
+  {
+    "id": 1,
+    "title": "title one",
+    "description": "description one",
+    "image": "image one"
+  },
+  {
+    "id": 2,
+    "title": "title two",
+    "description": "description two",
+    "image": "image two"
+  },
+  {
+    "id": 3,
+    "title": "title three",
+    "description": "description three",
+    "image": "image three"
+  },
+  {
+    "id": 4,
+    "title": "title four",
+    "description": "description four",
+    "image": "image four"
+  },
+];
+
+function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    const results = Card.filter(card =>
+      card.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+
+  return (
+    <div>
+      <input type="text" placeholder="Search" value={searchTerm} onChange={handleChange} />
+      <ul>
+        {searchResults.map(item => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SearchBar;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,47 @@
 import React from "react";
-
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import '../styles/NavBar.css'
+import { Link } from "react-router-dom";
+// BrowserRouter, Route,
 
 function Navbar() {
     return (
 
 
-        <nav className="nav">
-            <ul>
+        <div className="nav-container">
+            <nav>
+                <ul>
 
-                <li class="nav-item"> <a> <Link to="/">Home</Link> </a></li>
+                    <li class="nav-item"> 
+                    {/* <a> */}
+                         <Link to="/">Home</Link> 
+                         {/* </a> */}
+                         </li>
 
-                <li class="nav-item"> <a><Link to="/contact">Contact</Link> </a> </li>
+                    <li class="nav-item">
+                         {/* <a> */}
+                        <Link to="/contact">Contact</Link> 
+                        {/* </a>  */}
+                        </li>
 
-                <li class="nav-item"> <a> <Link to="/about">About</Link> </a> </li>
+                    <li class="nav-item"> 
+                    {/* <a> */}
+                         <Link to="/about">About</Link> 
+                    {/* </a>  */}
+                         </li>
 
-                <li class="nav-item"> <a> <Link to="/Resources">Resources</Link> </a> </li>
+                    <li class="nav-item"> 
+                    {/* <a>  */}
+                        <Link to="/Resources">Resources</Link> 
+                        {/* </a>  */}
+                        </li>
 
-                <li class="nav-item"> <a> <Link to="/Signup">Volunteer</Link> </a> </li>
-
+                        <li class="nav-item"> <a> <Link to="/Signup">Volunteer</Link> </a> </li>
 
             </ul>
         </nav>
 
 
-
+</div>
 
     );
 }
