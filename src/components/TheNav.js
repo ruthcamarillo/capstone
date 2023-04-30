9296c82adb6f86bcedbdb195e5ce06f0783dac99
JorgeZmedina <114126984+JorgeZmedina@users.noreply.github.com>
2023-04-30 10:18:17 -0400

    finished nav bar

--- src/components/TheNav.js
+++ src/components/TheNav.js
@@ -1,56 +1,62 @@
-import React from "react";
-import '../styles/theNav.css'
-// import React, { useState } from "react";
-// import '../styles/NavBar.css'
+import React, { useEffect } from "react";
+import "../styles/theNav.css";
 import { Link } from "react-router-dom";
 
+function TheNav() {
+  useEffect(() => {
+    let menu = document.querySelector("#menu-icon");
+    let navbar = document.querySelector(".navbar");
 
-function TheNav () {
-//     let menu = document.querySelector('#menu-icon')
-// let navbar = document.querySelector('.navbar')
+    menu.onclick = () => {
+      console.log(`clicked`);
+      menu.classList.toggle("bx-x");
+      navbar.classList.toggle("open");
+    };
+  }, []);
 
-// menu.onclick = () => {
-//     console.log(`clicked`)
-//     menu.classList.toggle('bx-x');
-//     navbar.classList.toggle('open');
-// }
+  return (
+    <>
+      <div className="allnav">
+        <div className="navBar">
+          <div className="headerr">
+            <div className="logo">
+              <Link to="/">
+                <img src="/logo.png" alt="logo" class="logo"></img>
+              </Link>
+            </div>
+            <nav>
+              <ul class="navbar">
+                <li>
+                  <a href="/home" class="active">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="/contact">Contact</a>
+                </li>
+                <li>
+                  <a href="/about">About us</a>
+                </li>
+                <li>
+                  <a href="Resources">Resources</a>
+                </li>
+                <li>
+                  <a href="/signup">Volunteer</a>
+                </li>
+              </ul>
 
-    return (
-<>
-     {/* <div className="body">
-    <header> */}
-    <div className="header">
-        <div className="logo"><Link to="/"><img src="/logo.png" alt="logoimg" className="logo"></img></Link></div>
-    <nav>
-        <ul className="navbar">
-            <Link to="/" className="active navtxt">Home</Link>
-            <Link to="/contact" className="navtxt" >Contact</Link>
-            {/* <li className="navbar"><a href="#">Services</a></li> */}
-            <Link to="/about" className="navtxt">About</Link>
-            {/* <li className="navbar"><a href="#">Blog</a></li> */}
-            <Link to="/Resources" className="navtxt">Resources</Link>
-            {/* <li className="navbar"><a href="#">Contact</a></li> */}
-            <Link to="/Signup" className="navtxt">Volunteer</Link>
-        </ul>
+              <div class="main">
+                {/* <a href="#" class="user"><i class="ri-user-fill"></i>Sign In</a> */}
+                {/* <a href="#">Register</a> */}
 
-        <div className="main">
-            {/* <a href="#" className="user"><i className="ri-user-fill"></i>Sign In</a> */}
-            <Link to="Sign-In" className="user navtxt popup"><i className="ri-user-fill"></i>Sign In</Link>
-            {/* <a href="#">Register!</a> */}
-            <Link to="Sign-up" className="navtxt popup">Register</Link>
-            <div className="bx bx-menu" id="menu-icon"></div>
-
-            {/* <button onClick={() => setButtonPopup(true)}>Log In</button>
-            <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-            </Popup> */}
+                <div class="bx bx-menu" id="menu-icon"></div>
+              </div>
+            </nav>
+          </div>
         </div>
-    </nav>
-    {/* </header>
- </div> */}
- </div>
-</>
-    )
+      </div>
+    </>
+  );
 }
-
 
 export default TheNav;
